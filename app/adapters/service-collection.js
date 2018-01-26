e36f626abe4dfbf0b7c750d8bd1787e3774abcbe
John Cowen <jcowen@hashicorp.com>
2018-01-26 16:00:59 +0000

    Start to clean up a touch

--- app/adapters/service-collection.js
+++ app/adapters/service-collection.js
@@ -1,8 +1,5 @@
 import Adapter from './application';
 export default Adapter.extend({
-  findAll() {
-    return this._super(...arguments);
-  },
   urlForFindAll(id, modelName) {
     return `/${this.namespace}/internal/ui/services`;
   }
--- app/adapters/service.js
+++ app/adapters/service.js
@@ -1,8 +1,5 @@
 import Adapter from './application';
 export default Adapter.extend({
-  findRecord() {
-    return this._super(...arguments);
-  },
   urlForFindRecord(id, modelName) {
     return `/${this.namespace}/health/service/${id}`;
   }
